534796d66caeadff7213249a74a4a078849e3207
kendall willard <kendallwillard@hotmail.com>
2019-01-31 23:30:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function increaseRankBy(n) {
   var result = document.getElementById('app').querySelectorAll('ul.ranked-list li')
   
   for( let i = 0; i < result.length; i++ ) {
-    result[i] += parseInt(n)
+    result[i].innerHTML += parseInt(n)
   }
   return result
 }
