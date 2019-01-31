11b6941941b1275f30bf470ad50ec5bfd0fc0c9a
kendall willard <kendallwillard@hotmail.com>
2019-01-31 23:26:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+function getFirstSelector(selector) {
+  const result = document.querySelector(selector)
+  return result
+}
 
+function nestedTarget() {
+  const result = document.querySelector("#nested").querySelector(".target")
+  return result
+}
+
+function increaseRankBy(n) {
+  const result = document.getElementById('app').querySelectorAll('ul.ranked-list li')
+  
+  for( let i = 0; i < result.length; i++ ) {
+    parseInt(result[i].innerHTML) += parseInt(n)
+  }
+  return result
+}
+
+function deepestChild() {
+  
+}
